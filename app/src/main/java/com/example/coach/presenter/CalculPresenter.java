964e082e82237d331e0e06add9f8b4420a156c72
Brodecks <raphae.donikian@oulootk.fr>
2026-03-11 00:17:06 +0100

    S3b : sérialisation

--- app/src/main/java/com/example/coach/presenter/CalculPresenter.java
+++ app/src/main/java/com/example/coach/presenter/CalculPresenter.java
@@ -1,20 +1,29 @@
 package com.example.coach.presenter;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 import com.example.coach.contract.ICalculView;
 import com.example.coach.model.Profil;
+import com.google.gson.Gson;
 
 import java.util.Date;
 
 public class CalculPresenter {
     private ICalculView vue;
     private Profil profil;
-
+    private static final String NOM_FIC = "coach_fic";
+    private static final String PROFIL_CLE = "profil_json";
+    private Gson gson;
+    private SharedPreferences prefs;
     /**
      *
      * @param vue
      */
-    public CalculPresenter(ICalculView vue) {
+    public CalculPresenter(ICalculView vue, Context context) {
         this.vue = vue;
+        this.prefs = context.getSharedPreferences(NOM_FIC, Context.MODE_PRIVATE);
+        this.gson = new Gson();
     }
 
     /**
@@ -26,7 +35,27 @@ public class CalculPresenter {
      */
     public void creerProfil(Integer poids, Integer taille, Integer age,Integer sexe){
         profil = new Profil(poids, taille, age, sexe, new Date());
-
+        sauvergarderProfil(profil);
         vue.afficherResultat(profil.getImage(), profil.getImg(), profil.getMessage(), profil.normal());
+    }
+
+    /**
+     *
+     * @param profil
+     */
+    private void sauvergarderProfil(Profil profil){
+        String json = gson.toJson(profil);
+        prefs.edit().putString(PROFIL_CLE, json).apply();
+    }
+
+    /**
+     *
+     */
+    public void chargerProfil(){
+        String json = prefs.getString(PROFIL_CLE, null);
+        if(json!=null){
+            Profil profil = gson.fromJson(json, Profil.class);
+            vue.remplirChamps(profil.getPoids(), profil.getTaille(), profil.getAge(), profil.getSexe());
+        }
     }
 }
