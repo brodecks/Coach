eb7a4d4aa03c32c13d41f0453dc2b4a05fa3d0cd
brodeck <raphael.donikian@outlook.fr>
2026-03-10 16:17:09 +0100

    S2bis2 : 4.C halfway through

--- app/src/main/java/com/example/coach/presenter/CalculPresenter.java
+++ app/src/main/java/com/example/coach/presenter/CalculPresenter.java
@@ -5,12 +5,13 @@ import com.example.coach.model.Profil;
 
 public class CalculPresenter {
     private ICalculView vue;
+    private Profil profil;
 
     public CalculPresenter(ICalculView vue) {
         this.vue = vue;
     }
     public void creerProfil(Integer poids, Integer taille, Integer age,Integer sexe){
-        Profil profil = new Profil(poids, taille, age, sexe);
+        profil = new Profil(poids, taille, age, sexe);
 
         vue.afficherResultat(profil.getImage(), profil.getImg(), profil.getMessage(), profil.normal());
     }
--- app/src/main/java/com/example/coach/view/MainActivity.java
+++ app/src/main/java/com/example/coach/view/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.coach.view;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,8 +15,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.coach.R;
 import com.example.coach.contract.ICalculView;
+import com.example.coach.presenter.CalculPresenter;
 
 public class MainActivity extends AppCompatActivity implements ICalculView {
+    private EditText txtPoids;
+    private EditText txtTaille;
+    private EditText txtAge;
+    private RadioButton rdHomme;
+    private RadioButton rdFemme;
+    private TextView lblImg;
+    private ImageView imgSmiley;
+    private Button btnCalc;
+    private CalculPresenter presenter;
+
+
+    private void init(){
+        chargeObjetGraphiques();
+        presenter = new CalculPresenter(this);
+    }
+    private void chargeObjetGraphiques(){
+        txtPoids = (EditText) findViewById(R.id.txtPoids);
+        txtTaille = (EditText) findViewById(R.id.txtTaille);
+        txtAge = (EditText) findViewById(R.id.txtAge);
+        rdHomme = (RadioButton) findViewById(R.id.rdHomme);
+        rdFemme = (RadioButton) findViewById(R.id.rdFemme);
+        lblImg = (TextView) findViewById(R.id.lblImg);
+        imgSmiley = (ImageView) findViewById(R.id.imgSmiley);
+        btnCalc = (Button) findViewById(R.id.btnCalc);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +54,18 @@ public class MainActivity extends AppCompatActivity implements ICalculView {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        init();
     }
 
     @Override
     public void afficherResultat(String image, double img, String message, boolean normal) {
 
+    }
+
+    private void btnCalc_clic(){
+        Integer poids = 0, taille = 0, age = 0, sexe = 0;
+        poids = Integer.parseInt(txtPoids.getText().toString());
+        taille = Integer.parseInt(txtTaille.getText().toString());
+        age = Integer.parseInt(txtAge.getText().toString());
     }
 }
